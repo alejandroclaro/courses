8b72dea53a9be99632a5835a7f18e99136f0197c
Alejandro Claro <alejandro.claro@smartmatic.com>
2015-01-17 00:29:18 -0430

    * Implement CBC in cryptography I homework 2.

--- cs255-cryptography-i/hw2/decrypt.py
+++ cs255-cryptography-i/hw2/decrypt.py
@@ -3,6 +3,18 @@ from Crypto        import Random
 from Crypto.Util   import Counter
 from Crypto.Cipher import AES
 
+def xor_blocks(block1, block2):
+  return "".join([ chr(ord(x) ^ ord(y)) for (x, y) in zip(block1, block2) ])
+
+def compute_cbc(cipher, blocksize, iv, text):
+  blocks = [ text[i : (i + blocksize)] for i in range(0, len(text), blocksize) ]
+  result = xor_blocks(iv, cipher.decrypt(blocks[0]))
+
+  for i in range(1, len(blocks)):
+    result += xor_blocks(blocks[i - 1], cipher.decrypt(blocks[i]))
+
+  return result
+
 def pkcs5_unpad(text):
   return text[:-ord(text[-1])]
 
@@ -12,8 +24,8 @@ def decrypt(mode, key, text):
   result = ""
 
   if mode == "CBC":
-    cipher  = AES.new(key, AES.MODE_CBC, IV=iv)
-    result  = cipher.decrypt(text)
+    cipher  = AES.new(key)
+    result  = compute_cbc(cipher, AES.block_size, iv, text)
     result  = pkcs5_unpad(result)
   elif mode == "CTR":
     ctr     = Counter.new(128, initial_value=int(iv.encode("hex"), 16))
